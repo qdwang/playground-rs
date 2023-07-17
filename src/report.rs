234c97557d15f4995b4aa3b9e8106cc76ca681f1
qdwang <wangqidong@gmail.com>
2023-07-17 18:15:01 +0800

    mod report

--- src/report.rs
+++ src/report.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 pub(crate) struct Report {
-    location: core::panic::Location<'static>,
+    loc: core::panic::Location<'static>,
     err: Box<dyn Error>,
 }
 pub(crate) trait ToReport<T> {
@@ -11,9 +11,9 @@ impl<T, E: std::error::Error + 'static> ToReport<T> for Result<T, E> {
     fn to_report(self) -> Result<T, Report> {
         match self {
             Ok(t) => Ok(t),
-            Err(e) => Err(Report {
-                location: *core::panic::Location::caller(),
-                err: e.into(),
+            Err(err) => Err(Report {
+                loc: *core::panic::Location::caller(),
+                err: err.into(),
             }),
         }
     }
@@ -23,19 +23,25 @@ impl std::fmt::Debug for Report {
         write!(
             f,
             "{}:{} -> {:?}",
-            self.location.file(),
-            self.location.line(),
+            self.loc.file(),
+            self.loc.line(),
             self.err
         )
     }
 }
 impl std::fmt::Display for Report {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(
+            f,
+            "{}:{} -> {}",
+            self.loc.file(),
+            self.loc.line(),
+            self.err
+        )
     }
 }
 impl Error for Report {
-    /// This method will ignore the report stack and get the first real source in ErrLog
+    /// This method will ignore the report stack and get the first real source in Report
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self.err.downcast_ref::<Report>() {
             Some(e) => e.source(),
