cf5cd249ec5ef888d674760e402f4c12d12d2b07
qdwang <wangqidong@gmail.com>
2023-07-24 12:11:37 +0800

    update bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,106 +1,27 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use wide::*;
 
-fn method1(w: u16, h: u16, data: &[u16]) -> Vec<u16> {
-    let w = w as usize;
-    let h = h as usize;
-    let get = |i: usize| unsafe { *data.get_unchecked(i) };
-
-    data.iter()
-        .enumerate()
-        .map(|(i, _)| {
-            let x = i % w;
-            let y = i / w;
-            let is_edge = y == 0 || y == h - 1 || x == 0 || x == w - 1;
-
-            if is_edge {
-                0
-            } else {
-                let i = i as usize;
-                get(i - w - 1)
-                    + get(i - w)
-                    + get(i - w + 1)
-                    + get(i - 1)
-                    + get(i)
-                    + get(i + 1)
-                    + get(i + w - 1)
-                    + get(i + w)
-                    + get(i + w + 1)
-            }
-        })
-        .collect()
+fn sum1(x: &[u16]) -> u128 {
+    x.into_iter().map(|x| *x as u128).sum::<u128>()
 }
-
-fn method2(w: u16, h: u16, data: &[u16]) -> Vec<u16> {
-    let w = w as usize;
-    let h = h as usize;
-    data.iter()
-        .enumerate()
-        .map(|(i, _)| {
-            let x = i % w;
-            let y = i / w;
-            let is_edge = y == 0 || y == h - 1 || x == 0 || x == w - 1;
-
-            if is_edge {
-                0
-            } else {
-                let i = i as usize;
-                assert!(i < data.len());
-                data[i - w - 1]
-                    + data[i - w]
-                    + data[i - w + 1]
-                    + data[i - 1]
-                    + data[i]
-                    + data[i + 1]
-                    + data[i + w - 1]
-                    + data[i + w]
-                    + data[i + w + 1]
-            }
+fn sum2(x: &[u16]) -> u128 {
+    x.chunks_exact(4)
+        .map(|arr| {
+            let v = [arr[0] as u32, arr[1] as u32, arr[2] as u32, arr[3] as u32];
+            u32x4::from(v)
         })
-        .collect()
-}
-
-fn method3(w: u16, h: u16, data: &[u16]) -> Vec<u16> {
-    let w = w as usize;
-    let h = h as usize;
-    let get = |i: usize| {
-        assert!(i < data.len());
-        data[i]
-    };
-
-    data.iter()
-        .enumerate()
-        .map(|(i, _)| {
-            let x = i % w;
-            let y = i / w;
-            let is_edge = y == 0 || y == h - 1 || x == 0 || x == w - 1;
-
-            if is_edge {
-                0
-            } else {
-                let i = i as usize;
-                get(i - w - 1)
-                    + get(i - w)
-                    + get(i - w + 1)
-                    + get(i - 1)
-                    + get(i)
-                    + get(i + 1)
-                    + get(i + w - 1)
-                    + get(i + w)
-                    + get(i + w + 1)
-            }
-        })
-        .collect()
+        .fold(u32x4::ZERO, |acc, x| acc + x)
+        .as_array_ref()
+        .map(|x| x as u128)
+        .into_iter()
+        .sum::<u128>()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let w = 300u16;
-    let h = 100u16;
+    let data: Vec<u16> = (0..4 * 10213u16).collect();
 
-    let data: Vec<u16> = (0..w * h).collect();
-
-    c.bench_function("method1", |b| b.iter(|| method1(w, h, &data)));
-    c.bench_function("method2", |b| b.iter(|| method2(w, h, &data)));
-    c.bench_function("method3", |b| b.iter(|| method3(w, h, &data)));
+    c.bench_function("sum1", |b| b.iter(|| sum1(black_box(&data))));
+    c.bench_function("sum2", |b| b.iter(|| sum2(black_box(&data))));
 }
 
 criterion_group!(benches, criterion_benchmark);
